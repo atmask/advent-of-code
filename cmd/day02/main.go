6ee1094db647d14ec45e5224d527530167ddc6a6
atmask <benjaminajm@gmail.com>
2025-12-04 09:03:03 -0500

    WIP: day02

--- cmd/day02/main.go
+++ cmd/day02/main.go
@@ -10,6 +10,29 @@ import (
 	"strings"
 )
 
+func rangeSum(min int, max int){
+	// Return the sum of values between max and min 
+	return ((max - min +1) * (max + min)) / 2
+}
+
+func getIterableMin(rangeMin int) int {
+	/*
+  if AB < CD:
+    MIN is (AB-1)(AB-1)
+  elif AB >= CD:
+    MIN is ABAB  
+  */ 
+	return -1
+}
+
+func getIterableMax(rangeMax int) int {
+	return -1
+}
+
+func getSolvableRanges(min int, max int) {
+  return
+}
+
 func main() {
 	/*
 				so...
@@ -43,6 +66,23 @@ func main() {
 		
 		// Clean up string
 		range = strings.TrimSpace(strings.TrimSuffix(range, ","))
+		
+		// Get range min and max
+		parts := strings.Split(s, "-")
+		rangeMin := parts[0]
+		rangeMax := parts[1]
+		solvableRanges = []int
+		if len(rangeMin) != len(rangeMax) {
+			getSolvableRanges(rangeMin, rangeMax)
+		} else {
+			solvableRanges := [rangeMin, rangeMax]
+	  }
+		
+		
+
+		// 
+
+
 
 		strlen := len(range)
 		if (strlen % 2) != 0 {
@@ -55,6 +95,21 @@ func main() {
 		fmt.Printf("First: %s, Last: %s\n", first, last)
 
 		// Smarter way would be to generate the numbers in the range rather than scanning
+		/*
+			double range
+
+			For 1234 - 6789
+
+			you generate all number x_1,y_1 that are greater than 1234 and lees than 6789.
+			if x_1, y_1 == 1 2 (i.e. range min) then start at min 34
+				12
+				12
+			
+			if x_1, y_1 == 67 then stop at 89
+
+		*/
+
+		// For a range with fixed length 12345678. This would be all 1234 that are greater than min and less than max
 		
 		// EOF
 		if err != nil {
@@ -62,3 +117,60 @@ func main() {
 		}
 	}
 }
+
+
+/*
+ex. 1234 - 5545
+
+two digits
+
+
+## Start
+3412
+1234 determines the min by it's first two digits. They must be larger than the second two though to be in range
+
+if AB < CD:
+  MIN is (AB-1)(AB-1)
+elif AB >= CD:
+  MIN is ABAB  
+
+
+12 12 (Bad)
+13 (good)
+
+## End 
+5545 determines the max it's first two but they must be less than 45
+
+This means that 
+if EF =< GH:
+  MAX is EFEF
+elif EF > GH:
+  MAX is (EF-1)(EF-1)
+  
+
+55
+
+
+ex. 3456 - 5412
+
+
+
+ex. 3412 - 5000
+
+34
+35 
+36
+37
+..
+49
+
+
+ex. 3415 - 12345
+
+34
+
+
+ex 3415 - 123456
+
+
+*/
