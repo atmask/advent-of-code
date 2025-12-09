67b7e59c087b27c250a05439ff112e1002da17df
atmask <benjaminajm@gmail.com>
2025-12-08 21:38:47 -0500

    Day02 pt 1 working

--- cmd/day02/main.go
+++ cmd/day02/main.go
@@ -7,30 +7,73 @@ import (
 	"log"
 	"os"
 
+	"strconv"
 	"strings"
 )
 
-func rangeSum(min int, max int){
-	// Return the sum of values between max and min 
-	return ((max - min +1) * (max + min)) / 2
+func rangeSum(min int, max int) int {
+	// Return the sum of values between max and min
+	return ((max - min + 1) * (max + min)) / 2
 }
 
-func getIterableMin(rangeMin int) int {
+func getIterableMin(rangeMin string) int {
 	/*
-  if AB < CD:
-    MIN is (AB-1)(AB-1)
-  elif AB >= CD:
-    MIN is ABAB  
-  */ 
-	return -1
+		if AB < CD:
+		MIN is (AB+1)(AB+1)
+		elif AB >= CD:
+		MIN is ABAB
+	*/
+
+	// Handle odd-digit numbers: jump to next even digit count
+	if len(rangeMin)%2 == 1 {
+		halfDigits := (len(rangeMin) + 1) / 2
+		if halfDigits == 1 {
+			return 1 // smallest half for 2-digit doubled (11)
+		}
+		// Return 10^(halfDigits-1), e.g., for 3 digits → return 10 (for 1010)
+		min := 1
+		for i := 0; i < halfDigits-1; i++ {
+			min *= 10
+		}
+		return min
+	}
+
+	firstHalf, _ := strconv.Atoi(rangeMin[:len(rangeMin)/2])
+	secondHalf, _ := strconv.Atoi(rangeMin[len(rangeMin)/2:])
+
+	if firstHalf < secondHalf {
+		return firstHalf + 1
+	}
+	return firstHalf
 }
 
-func getIterableMax(rangeMax int) int {
-	return -1
-}
+func getIterableMax(rangeMax string) int {
+	// if EF =< GH:
+	//   MAX is EFEF
+	// elif EF > GH:
+	//   MAX is (EF-1)(EF-1)
 
-func getSolvableRanges(min int, max int) {
-  return
+	// Handle odd-digit numbers: use previous even digit count's max
+	if len(rangeMax)%2 == 1 {
+		halfDigits := (len(rangeMax) - 1) / 2
+		if halfDigits == 0 {
+			return 0 // No valid doubled numbers below single-digit numbers
+		}
+		// Return 10^halfDigits - 1, e.g., for 3 digits → return 9 (for 99)
+		max := 1
+		for i := 0; i < halfDigits; i++ {
+			max *= 10
+		}
+		return max - 1
+	}
+
+	firstHalf, _ := strconv.Atoi(rangeMax[:len(rangeMax)/2])
+	secondHalf, _ := strconv.Atoi(rangeMax[len(rangeMax)/2:])
+
+	if firstHalf <= secondHalf {
+		return firstHalf
+	}
+	return firstHalf - 1
 }
 
 func main() {
@@ -57,120 +100,38 @@ func main() {
 	}
 
 	reader := bufio.NewReader(file)
+	nums := make([]int, 0)
+	sum := 0
 	for {
-		range, err := reader.ReadString(',')
+		idRange, err := reader.ReadString(',')
 
-		if range != "" {
-			fmt.Printf("Current Range: %s\n", range)
+		if idRange != "" {
+			fmt.Printf("Current Range: %s\n", idRange)
 		}
-		
+
 		// Clean up string
-		range = strings.TrimSpace(strings.TrimSuffix(range, ","))
-		
+		idRange = strings.TrimSpace(strings.TrimSuffix(idRange, ","))
+
 		// Get range min and max
-		parts := strings.Split(s, "-")
+		parts := strings.Split(idRange, "-")
 		rangeMin := parts[0]
 		rangeMax := parts[1]
-		solvableRanges = []int
-		if len(rangeMin) != len(rangeMax) {
-			getSolvableRanges(rangeMin, rangeMax)
-		} else {
-			solvableRanges := [rangeMin, rangeMax]
-	  }
-		
-		
 
-		// 
+		iterMin := getIterableMin(rangeMin)
+		iterMax := getIterableMax(rangeMax)
 
-
-
-		strlen := len(range)
-		if (strlen % 2) != 0 {
-			fmt.Println("This cannot be a mirrored range")
+		for i := iterMin; i <= iterMax; i++ {
+			doubled := fmt.Sprintf("%d%d", i, i)
+			val, _ := strconv.Atoi(doubled)
+			nums = append(nums, val)
+			sum += val
 		}
 
-		half := strlen / 2
-		first := range[:half]
-		last := range[half:]
-		fmt.Printf("First: %s, Last: %s\n", first, last)
-
-		// Smarter way would be to generate the numbers in the range rather than scanning
-		/*
-			double range
-
-			For 1234 - 6789
-
-			you generate all number x_1,y_1 that are greater than 1234 and lees than 6789.
-			if x_1, y_1 == 1 2 (i.e. range min) then start at min 34
-				12
-				12
-			
-			if x_1, y_1 == 67 then stop at 89
-
-		*/
-
-		// For a range with fixed length 12345678. This would be all 1234 that are greater than min and less than max
-		
 		// EOF
 		if err != nil {
-			log.Fatal(err)
+			break
 		}
 	}
+	fmt.Printf("Sum of nums: %d\nThere were %d bad ids", sum, len(nums))
+
 }
-
-
-/*
-ex. 1234 - 5545
-
-two digits
-
-
-## Start
-3412
-1234 determines the min by it's first two digits. They must be larger than the second two though to be in range
-
-if AB < CD:
-  MIN is (AB-1)(AB-1)
-elif AB >= CD:
-  MIN is ABAB  
-
-
-12 12 (Bad)
-13 (good)
-
-## End 
-5545 determines the max it's first two but they must be less than 45
-
-This means that 
-if EF =< GH:
-  MAX is EFEF
-elif EF > GH:
-  MAX is (EF-1)(EF-1)
-  
-
-55
-
-
-ex. 3456 - 5412
-
-
-
-ex. 3412 - 5000
-
-34
-35 
-36
-37
-..
-49
-
-
-ex. 3415 - 12345
-
-34
-
-
-ex 3415 - 123456
-
-
-*/
